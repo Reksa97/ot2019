05ce22001c06525ca401c4cce490a70ce2daf3a6
Reko Kälkäjä <reko.kalkaja@gmail.com>
2019-03-26 00:26:41 +0200

    project added and telegram bot initialized

---
+++ TelegramBot/src/main/java/reko/telegrambot/Main.java
@@ -0,0 +1,21 @@
+package reko.telegrambot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        
+        try {
+            botsApi.registerBot(new PizzaCounterBot());
+        } catch (TelegramApiException e) {
+            System.out.println(e.toString());
+        }
+    }
+    
+}
---
+++ TelegramBot/src/main/java/reko/telegrambot/PizzaCounterBot.java
@@ -0,0 +1,34 @@
+package reko.telegrambot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class PizzaCounterBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                System.out.println(e.toString());
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "pizza_counter_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "";
+    }
+}
